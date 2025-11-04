c8f6c59bff9a62184fead5d65d92a36cd1872a52
Padakalla Nagendra <youremail@example.com>
2025-11-04 08:49:11 +0530

    config issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-react-app/', // 👈 IMPORTANT: matches your repo name
   plugins: [
     react({
       babel: {
